8f22c5432be1a584ce0b1bb6d855b7bc1e7ee547
vinod.pahuja <vinod.pahuja@1c88fc3c-09b5-11df-a26f-8b716316a5bc>
2010-07-28 09:26:25 +0000

    Total Refactor

--- MyWTApp/src/rnd/webapp/mwtapp/client/MyApplicationHelper.java
+++ MyWTApp/src/rnd/webapp/mwtapp/client/MyApplicationHelper.java
@@ -3,6 +3,7 @@ package rnd.webapp.mwtapp.client;
 import pmt.client.module.helper.PMTModuleHelper;
 import rnd.mywt.client.application.AbstractApplicationHelper;
 
+
 public class MyApplicationHelper extends AbstractApplicationHelper {
 
 	@Override
