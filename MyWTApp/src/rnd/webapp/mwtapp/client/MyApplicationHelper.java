3d7216f0d193ff8b165399114eb5d0b2f0ce09b3
vinod.pahuja <vinod.pahuja@1c88fc3c-09b5-11df-a26f-8b716316a5bc>
2010-08-05 13:41:05 +0000

    MyWTApp reloaded

--- MyWTApp/src/rnd/webapp/mwtapp/client/MyApplicationHelper.java
+++ MyWTApp/src/rnd/webapp/mwtapp/client/MyApplicationHelper.java
@@ -2,13 +2,14 @@ package rnd.webapp.mwtapp.client;
 
 import pmt.client.module.helper.PMTModuleHelper;
 import rnd.mywt.client.application.AbstractApplicationHelper;
+import aa.client.module.AAModuleHelper;
 
 
 public class MyApplicationHelper extends AbstractApplicationHelper {
 
 	@Override
 	public void initialiseApplication() {
-//		addModuleHelper(new AAModuleHelper());
+		addModuleHelper(new AAModuleHelper());
 		addModuleHelper(new PMTModuleHelper());
 	}
 
